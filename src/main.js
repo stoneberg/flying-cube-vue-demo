4fd5e6f4857154f554a03eff879e4a6121dba4b1
Lee YuPyeong <stoneberg73@gmail.com>
2021-02-05 22:27:07 +0900

    layout bug fix

--- src/main.js
+++ src/main.js
@@ -2,12 +2,12 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import DefaultLayout from './components/layout/DefaultLayout.vue';
-import SimpleLayout from './components/layout/SimpleLayout.vue';
+import Default from './components/layout/DefaultLayout.vue';
+import Simple from './components/layout/SimpleLayout.vue';
 
 const app = createApp(App);
 app.use(store);
 app.use(router);
-app.component('Default', DefaultLayout);
-app.component('Simple', SimpleLayout);
+app.component('DefaultLayout', Default);
+app.component('SimpleLayout', Simple);
 app.mount('#app');
